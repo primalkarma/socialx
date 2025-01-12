71e9fdb3c90e12bd1310d2cb00667ec2a0f730c0
primalkarma <primalkarma@gmail.com>
2025-01-12 04:05:41 +0000

    make the skeleton

---
+++ app/page.tsx
@@ -0,0 +1,12 @@
+import { Button } from "@/components/ui/button";
+import Image from "next/image";
+
+const Home = () => {
+  return (
+    <>
+      <Button>Hello</Button>
+    </>
+  );
+}
+
+export default Home;
